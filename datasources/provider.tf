50516f6e7b057349c4687d42b666dfc7d529cfe7
saikumar <wwwsaikumar150@gmail.com>
2025-10-16 22:31:29 +0530

    devops conversation

---
+++ datasources/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.16.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+  # Configuration options
